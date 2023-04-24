611c4673a96edacd5485b7c294ef7f0208205dc3
Anastasia <fioB7@yandex.ru>
2023-04-24 21:31:02 +0500

    небольшой рефакторинг

--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -13,14 +13,18 @@ type ShortDate = {year: number, month: number, day: number};
 export class EmployeeComponent implements AfterViewInit {
 
     imgPath: string = "../../../assets/img/sample_photo.png";
+
     defaultBirthDate: ShortDate = { year: 2000, month: 2, day: 1};
-    age: number = this.calculateAgeDifference(this.defaultBirthDate).year;
-    ageWord: string = this.declineYearWord(this.age);
     defaultFirstWorkDate: ShortDate = { year: 2020, month: 10, day: 17};
-    yearWorkExperience: number = this.calculateAgeDifference(this.defaultFirstWorkDate).year;
+
+    age: number = this.calculateDateDifference(this.defaultBirthDate).year;
+    ageWord: string = this.declineYearWord(this.age);
+
+    yearWorkExperience: number = this.calculateDateDifference(this.defaultFirstWorkDate).year;
     yearExperienceWord: string = this.declineYearWord(this.yearWorkExperience);
-    monthWorkExperience: number = this.calculateAgeDifference(this.defaultFirstWorkDate).month;
+    monthWorkExperience: number = this.calculateDateDifference(this.defaultFirstWorkDate).month;
     monthExperienceWord: string = this.declineMonthWord(this.monthWorkExperience);
+
     employeeForm: FormGroup = new FormGroup({
         "employeeName": new FormControl("Васий Пупупипкин Васильевич", [
             Validators.required
@@ -38,11 +42,11 @@ export class EmployeeComponent implements AfterViewInit {
 
     ngAfterViewInit() {
         this.employeeForm.get("employeeBirth")?.valueChanges.subscribe((newBirthDate) => {
-            this.age = this.calculateAgeDifference(newBirthDate).year;
+            this.age = this.calculateDateDifference(newBirthDate).year;
             this.ageWord = this.declineYearWord(this.age);
         })
         this.employeeForm.get("employeeFirstWorkDay")?.valueChanges.subscribe((newStartDate) => {
-            let dateDifference = this.calculateAgeDifference(newStartDate);
+            let dateDifference = this.calculateDateDifference(newStartDate);
             this.yearWorkExperience = dateDifference.year;
             if (this.yearWorkExperience > 0) {
                 this.yearExperienceWord = this.declineYearWord(this.yearWorkExperience);
@@ -54,19 +58,15 @@ export class EmployeeComponent implements AfterViewInit {
         })
     }
 
-    calculateAgeDifference(newStartDate: ShortDate): ShortDate {
+    calculateDateDifference(newStartDate: ShortDate): ShortDate {
         let now = new Date();
         let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
         let startDate = new Date(newStartDate.year, newStartDate.month, newStartDate.day);
         let nextAnniversaryDay = new Date(today.getFullYear(), startDate.getMonth(), startDate.getDate());
         let yearDifference = today.getFullYear() - startDate.getFullYear();
-        if (today < nextAnniversaryDay) {
-            yearDifference -= 1;
-        }
+        yearDifference = today < nextAnniversaryDay ? yearDifference - 1 : yearDifference;
         let monthDifference = Math.abs(today.getMonth() - startDate.getMonth());
-        if (today.getMonth() < nextAnniversaryDay.getMonth()) {
-            monthDifference -= 1;
-        }
+        monthDifference = today.getMonth() < nextAnniversaryDay.getMonth() ? monthDifference - 1 : monthDifference;
         let dayDifference = Math.abs(today.getDay() - startDate.getDay());
         return {year: yearDifference, month: monthDifference, day: dayDifference};
     }
