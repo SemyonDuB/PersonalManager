e4e1cc4bda43ac8a5c71d10c4d10cd3575e5b991
Anastasia <fioB7@yandex.ru>
2023-05-14 14:26:55 +0500

    рефакторинг из требований eslint

--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -1,7 +1,7 @@
-import { ChangeDetectionStrategy, Component, AfterViewInit, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from "@angular/forms";
-import { TuiDay } from "@taiga-ui/cdk";
-import { Subscription } from "rxjs";
+import {ChangeDetectionStrategy, Component, AfterViewInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {TuiDay} from "@taiga-ui/cdk";
+import {Subscription} from "rxjs";
 
 type ShortDate = {year: number, month: number, day: number};
 type IntervalControlNames = {startName: string, endName: string};
@@ -15,36 +15,36 @@ type NamedIntervalControlNames = IntervalControlNames & {textName: string};
 })
 export class EmployeeComponent implements AfterViewInit {
 
-    imgPath: string = "../../../assets/img/sample_photo.png";
+    public imgPath: string = "../../../assets/img/sample_photo.png";
 
-    defaultBirthDate: ShortDate = { year: 2000, month: 2, day: 1};
-    defaultFirstWorkDate: ShortDate = { year: 2020, month: 10, day: 17};
+    public defaultBirthDate: ShortDate = { year: 2000, month: 2, day: 1};
+    public defaultFirstWorkDate: ShortDate = { year: 2020, month: 10, day: 17};
 
-    age: number = this.calculateDateDifference(this.defaultBirthDate).year;
-    ageWord: string = this.declineYearWord(this.age);
+    public age: number = this.calculateDateDifference(this.defaultBirthDate).year;
+    public ageWord: string = this.declineYearWord(this.age);
 
-    yearWorkExperience: number = this.calculateDateDifference(this.defaultFirstWorkDate).year;
-    yearExperienceWord: string = this.declineYearWord(this.yearWorkExperience);
-    monthWorkExperience: number = this.calculateDateDifference(this.defaultFirstWorkDate).month;
-    monthExperienceWord: string = this.declineMonthWord(this.monthWorkExperience);
+    public yearWorkExp: number = this.calculateDateDifference(this.defaultFirstWorkDate).year;
+    public yearExpWord: string = this.declineYearWord(this.yearWorkExp);
+    public monthWorkExp: number = this.calculateDateDifference(this.defaultFirstWorkDate).month;
+    public monthExpWord: string = this.declineMonthWord(this.monthWorkExp);
 
-    basicVacationControlNames: IntervalControlNames = {startName: "vacationStart", endName: "vacationEnd"}
-    vacationsHistory: Array<IntervalControlNames> = new Array<IntervalControlNames>({
+    public basicVacationControlNames: IntervalControlNames = {startName: "vacationStart", endName: "vacationEnd"};
+    public vacationsHistory: IntervalControlNames[] = new Array<IntervalControlNames>({
         startName: this.basicVacationControlNames.startName + "0",
         endName: this.basicVacationControlNames.endName + "0"
     });
-    vacationSubs: Subscription[] = [];
+    public vacationSubs: Subscription[] = [];
 
-    basicVacancyControlNames: NamedIntervalControlNames =
-        {startName: "vacancyStart", endName: "vacancyEnd", textName: "vacancyName"}
-    vacanciesHistory: Array<NamedIntervalControlNames> = new Array<NamedIntervalControlNames>({
+    public basicVacancyControlNames: NamedIntervalControlNames =
+        {startName: "vacancyStart", endName: "vacancyEnd", textName: "vacancyName"};
+    public vacanciesHistory: NamedIntervalControlNames[] = new Array<NamedIntervalControlNames>({
         startName: this.basicVacancyControlNames.startName + "0",
         endName: this.basicVacancyControlNames.endName + "0",
         textName: this.basicVacancyControlNames.textName + "0"
     });
-    vacancySubs: Subscription[] = [];
+    public vacancySubs: Subscription[] = [];
 
-    employeeForm: FormGroup = new FormGroup({
+    public employeeForm: FormGroup = new FormGroup({
         "employeeName": new FormControl("Васий Пупупипкин Васильевич", [
             Validators.required
         ]),
@@ -64,21 +64,21 @@ export class EmployeeComponent implements AfterViewInit {
         "vacancyName0" : new FormControl("Уборщик главного зала")
     });
 
-    ngAfterViewInit() {
-        this.employeeForm.get("employeeBirth")?.valueChanges.subscribe((newBirthDate) => {
+    public ngAfterViewInit(): void {
+        this.employeeForm.get("employeeBirth")?.valueChanges.subscribe((newBirthDate: ShortDate) => {
             this.age = this.calculateDateDifference(newBirthDate).year;
             this.ageWord = this.declineYearWord(this.age);
         });
 
-        this.employeeForm.get("employeeFirstWorkDay")?.valueChanges.subscribe((newStartDate) => {
-            let dateDifference = this.calculateDateDifference(newStartDate);
-            this.yearWorkExperience = dateDifference.year;
-            if (this.yearWorkExperience > 0) {
-                this.yearExperienceWord = this.declineYearWord(this.yearWorkExperience);
+        this.employeeForm.get("employeeFirstWorkDay")?.valueChanges.subscribe((newStartDate: ShortDate) => {
+            const dateDifference: ShortDate = this.calculateDateDifference(newStartDate);
+            this.yearWorkExp = dateDifference.year;
+            if (this.yearWorkExp > 0) {
+                this.yearExpWord = this.declineYearWord(this.yearWorkExp);
             }
-            this.monthWorkExperience = dateDifference.month;
-            if (this.monthWorkExperience > 0) {
-                this.monthExperienceWord = this.declineMonthWord(this.monthWorkExperience);
+            this.monthWorkExp = dateDifference.month;
+            if (this.monthWorkExp > 0) {
+                this.monthExpWord = this.declineMonthWord(this.monthWorkExp);
             }
         });
 
@@ -90,43 +90,43 @@ export class EmployeeComponent implements AfterViewInit {
         this.subscribeVacancyFields(this.vacanciesHistory[0]!.textName, 1);
     }
 
-    subscribeVacationFields(controlName: string, nextIndex: number) {
-        let sub = this.employeeForm.get(controlName)?.valueChanges.subscribe(() => {
-            this.vacationSubs.forEach((sub) => sub.unsubscribe());
+    public subscribeVacationFields(controlName: string, nextIndex: number): void {
+        const sub: Subscription = this.employeeForm.get(controlName)?.valueChanges.subscribe(() => {
+            this.vacationSubs.forEach((oldSub: Subscription) => oldSub.unsubscribe());
             this.vacationSubs = [];
-            this.generateVacationFieldChanges(nextIndex);
-        });
-        this.vacationSubs.push(sub!);
+            this.generateVacationFields(nextIndex);
+        })!;
+        this.vacationSubs.push(sub);
     }
 
-    subscribeVacancyFields(controlName: string, nextIndex: number) {
-        let sub = this.employeeForm.get(controlName)?.valueChanges.subscribe(() => {
-            this.vacancySubs.forEach((sub) => sub.unsubscribe());
+    public subscribeVacancyFields(controlName: string, nextIndex: number): void {
+        const sub: Subscription = this.employeeForm.get(controlName)?.valueChanges.subscribe(() => {
+            this.vacancySubs.forEach((oldSub: Subscription) => oldSub.unsubscribe());
             this.vacancySubs = [];
-            this.generateVacancyFieldChanges(nextIndex);
-        });
+            this.generateVacancyFields(nextIndex);
+        })!;
         this.vacancySubs.push(sub!);
     }
 
-    addFormControls(...controlNames: string[]) {
-        controlNames.forEach((name) => {
+    public addFormControls(...controlNames: string[]): void {
+        controlNames.forEach((name: string) => {
             this.employeeForm.addControl(name, new FormControl());
         });
     }
 
-    generateVacationFieldChanges(index: number) {
-        let startName = this.basicVacationControlNames.startName + index.toString();
-        let endName = this.basicVacationControlNames.endName + index.toString();
+    public generateVacationFields(index: number): void {
+        const startName: string = this.basicVacationControlNames.startName + index.toString();
+        const endName: string = this.basicVacationControlNames.endName + index.toString();
         this.addFormControls(startName, endName);
         this.vacationsHistory.push({startName: startName, endName: endName});
         this.subscribeVacationFields(this.vacationsHistory[index]!.startName, index + 1);
         this.subscribeVacationFields(this.vacationsHistory[index]!.endName, index + 1);
     }
 
-    generateVacancyFieldChanges(index: number) {
-        let startName = this.basicVacancyControlNames.startName + index.toString();
-        let endName = this.basicVacancyControlNames.endName + index.toString();
-        let textName = this.basicVacancyControlNames.textName + index.toString();
+    public generateVacancyFields(index: number): void {
+        const startName: string = this.basicVacancyControlNames.startName + index.toString();
+        const endName: string = this.basicVacancyControlNames.endName + index.toString();
+        const textName: string = this.basicVacancyControlNames.textName + index.toString();
         this.addFormControls(startName, endName,textName);
         this.vacanciesHistory.push({startName: startName, endName: endName, textName: textName});
         this.subscribeVacancyFields(this.vacanciesHistory[index]!.startName, index + 1);
@@ -134,20 +134,21 @@ export class EmployeeComponent implements AfterViewInit {
         this.subscribeVacancyFields(this.vacanciesHistory[index]!.textName, index + 1);
     }
 
-    calculateDateDifference(newStartDate: ShortDate): ShortDate {
-        let now = new Date();
-        let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-        let startDate = new Date(newStartDate.year, newStartDate.month, newStartDate.day);
-        let nextAnniversaryDay = new Date(today.getFullYear(), startDate.getMonth(), startDate.getDate());
-        let yearDifference = today.getFullYear() - startDate.getFullYear();
+    public calculateDateDifference(newStartDate: ShortDate): ShortDate {
+        const now: Date = new Date();
+        const today: Date = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+        const startDate: Date = new Date(newStartDate.year, newStartDate.month, newStartDate.day);
+        const nextAnniversaryDay: Date = new Date(today.getFullYear(), startDate.getMonth(), startDate.getDate());
+        let yearDifference: number = today.getFullYear() - startDate.getFullYear();
         yearDifference = today < nextAnniversaryDay ? yearDifference - 1 : yearDifference;
-        let monthDifference = Math.abs(today.getMonth() - startDate.getMonth());
+        let monthDifference: number = Math.abs(today.getMonth() - startDate.getMonth());
         monthDifference = today.getMonth() < nextAnniversaryDay.getMonth() ? monthDifference - 1 : monthDifference;
-        let dayDifference = Math.abs(today.getDay() - startDate.getDay());
+        const dayDifference: number = Math.abs(today.getDay() - startDate.getDay());
+
         return {year: yearDifference, month: monthDifference, day: dayDifference};
     }
 
-    declineNumberWord(number: number, nominative: string, parental: string, plural: string): string {
+    public declineNumberWord(number: number, nominative: string, parental: string, plural: string): string {
         if (number.toString().slice(number.toString().length - 1) === '1') {
             return nominative;
         } else if ('234'.includes(number.toString().slice(number.toString().length - 1))) {
@@ -157,11 +158,11 @@ export class EmployeeComponent implements AfterViewInit {
         }
     }
 
-    declineYearWord(number: number): string {
+    public declineYearWord(number: number): string {
         return this.declineNumberWord(number, 'год', 'года', 'лет');
     }
 
-    declineMonthWord(number: number): string {
+    public declineMonthWord(number: number): string {
         return this.declineNumberWord(number, 'месяц', 'месяца', 'месяцев');
     }
 }
