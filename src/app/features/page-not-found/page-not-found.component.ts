58564cba2f727b2416a78e2c1c557e650eb1000b
Anastasia <fioB7@yandex.ru>
2023-05-16 20:30:27 +0500

    удалена папка styles у not-found

--- src/app/features/page-not-found/page-not-found.component.ts
+++ src/app/features/page-not-found/page-not-found.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'page-not-found',
     templateUrl: './page-not-found.component.html',
-    styleUrls: ['./styles/page-not-found.css']
+    styleUrls: ['./page-not-found.css']
 })
 export class PageNotFoundComponent {
 
