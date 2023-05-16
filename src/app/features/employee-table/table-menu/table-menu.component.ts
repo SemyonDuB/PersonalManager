131ae75814bcfeb722d54065e4cfc26e616c7918
Anastasia <fioB7@yandex.ru>
2023-05-16 21:26:29 +0500

    добавлены кнопки в меню таблицы

--- src/app/features/employee-table/table-menu/table-menu.component.ts
+++ src/app/features/employee-table/table-menu/table-menu.component.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./table-menu.component.css']
 })
 export class TableMenuComponent {
+
+    public isOpenFilters: boolean = false;
+
+    constructor() {
+    }
+
+    public toggleOpeningFilters(): void {
+        this.isOpenFilters = !this.isOpenFilters;
+    }
 }
