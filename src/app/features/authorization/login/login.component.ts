b57917a44ec20ad1920725739d9f40f0c0fd8915
Anastasia <fioB7@yandex.ru>
2023-06-04 17:16:41 +0500

    реализованы сообщения о статусе аутентификации

--- src/app/features/authorization/login/login.component.ts
+++ src/app/features/authorization/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../../core/services/auth.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthModalService } from '../../../core/services/auth-modal.service';
+import {MessagingService} from "../../../core/services/messaging.service";
 
 @Component({
     selector: 'app-login',
@@ -18,14 +19,20 @@ export class LoginComponent {
     });
 
     constructor(private readonly _authService: AuthService,
-                private readonly _authModalService: AuthModalService) {
+                private readonly _authModalService: AuthModalService,
+                private readonly _messagingService: MessagingService) {
     }
 
     public onSubmit(): void {
         this._authService
             .login(this.loginForm.value.username!, this.loginForm.value.password!)
-            .subscribe();
-        this._authModalService.changeModalOpening(false);
+            .subscribe((isSuccess: boolean): void => {
+                if (isSuccess) {
+                    this._authModalService.changeModalOpening(false);
+                } else {
+                    this._messagingService.sendModalMessage('Неверные имя или пароль', 5000);
+                }
+            });
     }
 
     public changeAuthType(): void {
